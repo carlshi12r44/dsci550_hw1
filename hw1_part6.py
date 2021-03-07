5a3e00a8ab8eec0f280b32b73945eea226e82250
Yifeng <coin123fs@gmail.com>
2021-03-06 22:47:18 -0800

    added process part 6

--- hw1_part6.py
+++ hw1_part6.py
@@ -5,14 +5,8 @@ import pandas
 import json
 import pandas as pd
 import config
-
-
-def reformat_dict(d: dict) -> dict:
-    '''
-    reformat dictionary from process_csv to dictinoary with another format
-
-    '''
-    return {}
+from urllib.parse import urlparse
+from pysafebrowsing import SafeBrowsing
 
 
 def process_gdp_csv(file_path: str) -> dict:
@@ -41,6 +35,31 @@ def find_gdp_per_capita(gdp_dict: dict, locations_info: dict) -> int:
     return -1
 
 
+def process_domain_names(domains_path: str) -> dict:
+    '''
+    process domain names 
+    '''
+    data_dict = json.loads(domains_path)
+    return data_dict
+
+
+def check_urls_malicious(urls_list: list) -> int:
+    '''
+    check if urls are malicious
+    1 means malicious, 0 means not malicious, -1 means either url is not present
+    '''
+    url_check_server = SafeBrowsing(config.google_safe_browsing_api_key)
+    if urls_list:
+        url = urls_list[0][0]
+        #url_domain = urlparse(url).netloc
+        response = url_check_server.lookup_urls([url])
+        if response[url]['malicious']:
+            return 1
+        else:
+            return 0
+    return -1
+
+
 def process_part_6(file_path: str) -> dict:
     '''
     find url from emails content 
@@ -51,15 +70,19 @@ def process_part_6(file_path: str) -> dict:
     GDP_path = os.getcwd() + "\data\gdp-per-capita-worldbank.csv"
     gdp_dict = process_gdp_csv(GDP_by_county_csv_path)
 
+    domain_names_json_path = os.getcwd() + "\data\world_universities_and_domains.json"
+
     # use regular expression to find the urls pattern
-    pattern = r"/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/"
+    pattern = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     for key in data.keys():
         print(key)
         content = data[key]["X-TIKA:content"]
-        data[key]["urls"] = re.findall(pattern, content)
         data[key]["gdpPerCapitaPerCountry"] = find_gdp_per_capita(
             gdp_dict, data[key]['locations'])
-
+        if (key == "2680"):
+            continue
+        data[key]["urls"] = re.findall(pattern, content)
+        data[key]["areUrlsMalicious"] = check_urls_malicious(data[key]["urls"])
     return data
 
 
@@ -68,4 +91,4 @@ if __name__ == "__main__":
 
     json_file_string_context_path = os.getcwd() + "\emails_context_5b.json"
     json_data_part6_data = process_part_6(json_file_string_context_path)
-    print(json_data_part6_data["7"]["gdpPerCapitaPerCountry"])
+    print(json_data_part6_data["7"]["urls"][0][0])
